c7562d1b7bace20b3e8dcc90da5689329b98f460
Ryanrob327 <ryanrob327@gmail.com>
2026-05-12 13:03:18 -0700

    setup complete

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,27 @@
-#include "raylib.h"
+#include <raylib.h>
+#include "ball.h"
 
-constexpr auto SCREEN_WIDTH  = 800;
-constexpr auto SCREEN_HEIGHT = 450;
-
-int main()
+int main() 
 {
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
+    const Color darkGreen = {20, 160, 133, 255};
+    
+    constexpr int screenWidth = 800;
+    constexpr int screenHeight = 600;
+    
+    Ball ball;
+    
+    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
     SetTargetFPS(60);
-
-    Texture2D texture = LoadTexture(ASSETS_PATH"test.png");
-
+    
     while (!WindowShouldClose())
     {
+        ball.Update();
+        
         BeginDrawing();
-
-        ClearBackground(RAYWHITE);
-
-        const int texture_x = SCREEN_WIDTH / 2 - texture.width / 2;
-        const int texture_y = SCREEN_HEIGHT / 2 - texture.height / 2;
-        DrawTexture(texture, texture_x, texture_y, WHITE);
-
-        const char* text = "OMG! IT WORKS!";
-        const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
-        DrawText(text, SCREEN_WIDTH / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
-
+            ClearBackground(darkGreen);
+            ball.Draw();
         EndDrawing();
     }
-
-    UnloadTexture(texture);
-
+    
     CloseWindow();
-    return 0;
 }
